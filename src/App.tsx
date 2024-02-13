a9e0b8cdd7f8c1efb51fe1c36e2c9b6ba89bce9b
Jeff Gouveia <jeff.gouveia@hotmail.com>
2024-02-13 17:50:37 -0400

    Added libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from '@/components/theme/ThemeProvider.tsx'
+import { queryClient } from '@/lib/react-query.ts'
 import router from '@/router.tsx'
 
 export default function App() {
@@ -14,7 +16,9 @@ export default function App() {
       <Toaster richColors />
 
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
