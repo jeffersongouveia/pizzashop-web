fa31eedda9f4e0de7e3e0729ef4ebee64f00f997
Jeff Gouveia <jeff.gouveia@hotmail.com>
2024-02-20 13:20:48 -0400

    Setup for unit tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import { defineConfig, UserConfig } from 'vite'
+import { InlineConfig } from 'vitest'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,11 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
